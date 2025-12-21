91f7ecb6fe9e2a256f1c8b5f6d79f621a736a598
Manuel Seitz <manuuseitz@example.com>
2025-12-21 20:58:41 +0000

    añadir reto 20 🎁

---
+++ retos/20_el_almacen_vertical/typescript.ts
@@ -0,0 +1,19 @@
+// 5 ⭐
+function dropGifts(warehouse: string[][], drops: number[]): string[][] {
+  const GIFT = "#";
+  const EMPTY = ".";
+  const warehouseCopy = warehouse.map((row) => [...row]);
+
+  for (const drop of drops) {
+    if (warehouseCopy[0][drop] === GIFT) continue;
+
+    for (let y = warehouseCopy.length - 1; y >= 0; y--) {
+      if (warehouseCopy[y][drop] === EMPTY) {
+        warehouseCopy[y][drop] = "#";
+        break;
+      }
+    }
+  }
+
+  return warehouseCopy;
+}
